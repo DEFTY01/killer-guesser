305379d42b011f47cd022cf3a84350ffe5be78b8
Bence Mucsi <originalmucsibence@gmail.com>
2026-03-09 13:39:37 +0000

    Fix blog upload

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.turso.io",
       },
+      {
+        protocol: "https",
+        hostname: "blob.vercel-storage.com",
+      },
     ],
   },
 };
